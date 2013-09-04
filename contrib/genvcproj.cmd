46a3964a5ec293fb5624805d7e07c2123ee6696a
occash <artiom.shal@gmail.com>
2013-09-04 17:58:16 +0400

    Fix script

--- contrib/genvcproj.cmd
+++ contrib/genvcproj.cmd
@@ -1,10 +1,12 @@
 set QTROOT=C:\Qt\4.8.4
 set SPEC=win32-msvc2010
 
-for /D /r %%G in ("*") do (
+cd ..\src
+
+for /D %%G in ("*") do (
 cd %%G
-%QTROOT%\bin\qmame -spec %SPEC% -tp vc %%G.pro
+%QTROOT%\bin\qmake -spec %SPEC% -tp vc %%G.pro
 cd ..
 )
 
-%QTROOT%\bin\qmame -spec %SPEC% -tp vc metasystem.pro
+%QTROOT%\bin\qmake -spec %SPEC% -tp vc metasystem.pro
